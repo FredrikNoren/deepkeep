b31a5f36eb6f20f87bedd4f830d4f73488061c6e
Fredrik Noren <noren@spotify.com>
2015-07-07 21:43:33 -0400

    Use heroku port

--- index.js
+++ index.js
@@ -285,4 +285,7 @@ app.get('/:username/:project', function(req, res) {
   });
 });
 
-app.listen(8080);
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
