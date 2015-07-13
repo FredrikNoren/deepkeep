5c9e480a2fc677731098bd90d8d2b062ebb8ac6f
Fredrik Noren <noren@spotify.com>
2015-07-12 23:11:01 -0400

    Add google analytics

--- components/Html.jsx
+++ components/Html.jsx
@@ -15,6 +15,15 @@ var Html = React.createClass({
           <div id="main" dangerouslySetInnerHTML={{__html: this.props.html}}></div>
           <script src="/bundle.js" />
           <script dangerouslySetInnerHTML={{__html: this.props.script}}></script>
+          <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-65047638-1', 'auto');
+            ga('send', 'pageview');
+          </script>
         </body>
       </html>);
   }
