704e66f2e1d102c5a80d5b187339084493f40784
Fredrik Noren <noren@spotify.com>
2015-07-13 23:23:46 -0400

    Add title

--- components/Html.jsx
+++ components/Html.jsx
@@ -5,14 +5,17 @@ var Html = React.createClass({
   render: function() {
     return (
       <html>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,800,400' rel='stylesheet' type='text/css' />
-        <link href='http://fonts.googleapis.com/css?family=Quicksand:300,400,700' rel='stylesheet' type='text/css' />
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" rel="stylesheet" />
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
-        <link rel="icon" type="image/png" href="static/favicon-32x32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="static/favicon-16x16.png" sizes="16x16" />
-        <link href="/bundle.css" rel="stylesheet" />
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,800,400' rel='stylesheet' type='text/css' />
+          <link href='http://fonts.googleapis.com/css?family=Quicksand:300,400,700' rel='stylesheet' type='text/css' />
+          <link href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" rel="stylesheet" />
+          <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
+          <link rel="icon" type="image/png" href="static/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="static/favicon-16x16.png" sizes="16x16" />
+          <link href="/bundle.css" rel="stylesheet" />
+          <title>deepkeep</title>
+        </head>
         <body>
           <div id="main" dangerouslySetInnerHTML={{__html: this.props.html}}></div>
           <script src="/bundle.js" />
