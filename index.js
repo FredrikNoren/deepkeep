853545e7d00786214c858c23d6a90c629bc0c742
Fredrik Noren <noren@spotify.com>
2015-07-17 22:12:41 -0400

    Move out storage classes

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ var pg = require('pg');
 var elasticsearch = require('elasticsearch');
 var React = require('react');
 require('node-jsx').install({extension: '.jsx', harmony: true });
+var FSStorage = require('./server/fsstorage');
+var S3Storage = require('./server/s3storage');
 
 var app = express();
 
@@ -214,58 +216,6 @@ var queries = {};
 queries.allProjects = new PartialQuery('select userid, projectname, username from cached_project_versions group by userid, projectname, username');
 queries.countAllProject = new PartialQuery('select count(*) as nprojects from ($[allProjects]) as allprojects', { allProjects: queries.allProjects });
 
-function FSStorage(app) {
-  if (!fs.existsSync('storage')) fs.mkdirSync('storage');
-  app.use('/storage', express.static('storage'));
-}
-FSStorage.prototype.exists = function(key) {
-  return Promise.resolve(fs.existsSync(path.join('storage', key)));
-}
-FSStorage.prototype.upload = function(key, stream) {
-  return new Promise(function(resolve, reject) {
-    var f = fs.createWriteStream(path.join('storage', key));
-    stream.pipe(f).on('close', function() {
-      resolve();
-    });
-  });
-}
-FSStorage.prototype.urlForKey = function(key) {
-  return '/storage/' + encodeURIComponent(key);
-}
-function S3Storage() {
-  var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-  var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-  this.S3_BUCKET = process.env.S3_BUCKET;
-  aws.config.update({ accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY });
-  this.s3 = new aws.S3({ params: { Bucket: this.S3_BUCKET }});
-}
-S3Storage.prototype.exists = function(key) {
-  return new Promise(function(resolve, reject) {
-    this.s3.headObject({ Key: key }, function(err, headRes) {
-      resolve(headRes || err.code !== 'NotFound');
-    });
-  }.bind(this));
-}
-S3Storage.prototype.upload = function(key, stream) {
-  return new Promise(function(resolve, reject) {
-    var s3obj = new aws.S3({
-      params: {
-        Bucket: this.S3_BUCKET,
-        Key: key,
-        ACL: 'public-read'
-      }
-    });
-    s3obj.upload({ Body: stream }).
-      on('httpUploadProgress', function(evt) { console.log(evt); }).
-      send(function(err, data) {
-        if (err) reject(err);
-        else resolve(data);
-      });
-  }.bind(this));
-}
-S3Storage.prototype.urlForKey = function(key) {
-  return 'https://' + this.S3_BUCKET + '.s3.amazonaws.com/' + encodeURIComponent(key);
-}
 var storage;
 if (process.env.AWS_ACCESS_KEY) storage = new S3Storage();
 else storage = new FSStorage(app);
